063a0020ca6e85f54dea6684f5b961031d7d84fb
vladimir borovic <vladimir.borovic@gmail.com>
2017-08-01 15:34:29 +0300

    Prepare test environment

---
+++ tests/specs/components/Button.spec.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import Button from '../../../src/components/Button'
+
+
+describe('(Component) Button', () => {
+  function button(props) {
+      const defaultProps = {
+        text: "doom",
+        onClick: undefined
+      };
+      return shallow(<Button {...Object.assign(defaultProps, props)} />);
+  }
+
+  it('renders without exploding', () => {
+    const wrapper = button();
+    expect(wrapper).to.have.length(1);
+    expect(wrapper.text()).to.be.equal("doom");
+  });
+
+  it('should call on click handler', () => {
+    const onClick = sinon.spy();
+    const wrapper = button({onClick});
+    wrapper.simulate('click');
+    expect(onClick.calledOnce).to.be.true;
+  });
+});
